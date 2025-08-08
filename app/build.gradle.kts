aecc54f35514401439aa993f44aa2ca60b8fc2f5
zakymzn <mamurzakynurrokhman@gmail.com>
2025-08-09 00:44:03 +0700

    codelab paging 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.recyclerview)
+    implementation(libs.androidx.paging.runtime.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/dicoding/mystudentdata/adapter/StudentListAdapter.kt
+++ app/src/main/java/com/dicoding/mystudentdata/adapter/StudentListAdapter.kt
@@ -2,14 +2,14 @@ package com.dicoding.mystudentdata.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.dicoding.mystudentdata.database.Student
 import com.dicoding.mystudentdata.databinding.ItemStudentBinding
 
 class StudentListAdapter :
-    ListAdapter<Student, StudentListAdapter.WordViewHolder>(WordsComparator()) {
+    PagedListAdapter<Student, StudentListAdapter.WordViewHolder>(WordsComparator()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
         val binding = ItemStudentBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -22,8 +22,8 @@ class StudentListAdapter :
 
     class WordViewHolder(private val binding: ItemStudentBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(data: Student) {
-            binding.tvItemName.text = data.name
+        fun bind(data: Student?) {
+            binding.tvItemName.text = data?.name
         }
     }
 
